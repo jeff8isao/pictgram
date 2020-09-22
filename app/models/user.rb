cd3452fa9893a7e92b4bf8799d38dd15be60dd4c
”名前” <>
2020-09-22 10:18:17 +0000

    Rails06課題の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   
+  
   has_secure_password
+  
+  has_many :topics
   
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, presence: true,
-                      length: { minimum: 8, maximum: 32, allow_blank: true },
+                      length: { minimum: 8, maximum: 32, },
                       format: {
                         with: VALID_PASSWORD_REGEX
                       },
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'topics/new'
   get 'sessions/new'
-  get 'users/new'
+  
   root 'pages#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/help'
   
@@ -9,4 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
 end
